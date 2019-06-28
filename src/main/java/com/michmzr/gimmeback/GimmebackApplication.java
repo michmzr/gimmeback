87c7ae56573e9bd7f1d83138c738378c8aff6ef4
Michał Mazur <michmzr@gmail.com>
2019-06-28 12:41:56 +0200

    Projekt init

---
+++ src/main/java/com/michmzr/gimmeback/GimmebackApplication.java
@@ -0,0 +1,13 @@
+package com.michmzr.gimmeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GimmebackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GimmebackApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/michmzr/gimmeback/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.michmzr.gimmeback;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GimmebackApplication.class);
+    }
+
+}
