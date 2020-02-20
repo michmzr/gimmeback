0492918de281bb053508f7a4d63fab258f2d4f26
michal.mazur <michal.mazur@pl.imshealth.com>
2020-02-20 17:54:18 +0100

    Updated ``.gitignore` Updated gradle files

--- src/main/java/com/michmzr/gimmeback/GimmeBackApplication.java
+++ src/main/java/com/michmzr/gimmeback/GimmeBackApplication.java
@@ -3,23 +3,16 @@ package com.michmzr.gimmeback;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.michmzr.gimmeback.core.audit.AuditorAwareImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class GimmeBackApplication {
-    @Bean
-    public AuditorAware<String> auditorAware() {
-        return new AuditorAwareImpl();
-    }
-
     @Bean
     @Primary
     public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
@@ -35,5 +28,4 @@ public class GimmeBackApplication {
     public static void main(String[] args) {
         SpringApplication.run(GimmeBackApplication.class, args);
     }
-
 }
